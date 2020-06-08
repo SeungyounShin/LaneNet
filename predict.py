6f6051375caf30e1b44dbdd71197a9afd17ef735
Seungyoun, Shin <ssy10011218@gmail.com>
2020-06-08 16:58:14 +0900

    clustering part update.

--- predict.py
+++ predict.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from sklearn.cluster import MeanShift,DBSCAN
+import hdbscan
 
 from dataloader import TUSimple
 from models.lanenet import LaneNet
@@ -20,11 +21,11 @@ batch_size = 1
 
 
 #model load
-model = torch.load("./ckpt/25.pth",map_location=torch.device('cpu')).eval()
+model = torch.load("./ckpt/25.pth").eval().cuda()
 model_name = model.__class__.__name__
 
 #dataset load
-dataset = TUSimple("./tusimple_part")
+dataset = TUSimple("/home/yo0n/바탕화면/TUsimple")
 loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                            shuffle=True, num_workers=0)
 print("dataset lenght :: ",len(dataset))
@@ -35,35 +36,46 @@ for iteration,sample in enumerate(loader):
     img = img.permute(0,3,1,2).float()/255.
     binary_label = binary_label.view(-1,1,368,640).float()
     instance_label = instance_label.view(-1,1,368,640).float()
-    #img,binary_label,instance_label = img.cuda(),binary_label.cuda(),instance_label.cuda()
+    img,binary_label,instance_label = img.cuda(),binary_label.cuda(),instance_label.cuda()
 
     start =time.time()
     binary_pred, instance_pred = model(img)
     end = time.time()
 
     binary_pred = np.argmax(F.softmax(binary_pred).squeeze().detach().cpu().numpy(), axis=0)
-    instance_pred = instance_pred.squeeze().detach().cpu().numpy()
-    mask = (instance_pred * np.stack([binary_pred, binary_pred], axis=0)) #(2, 368, 640)
-    lanes = instance_pred[np.stack([binary_pred, binary_pred], axis=0) > 0].reshape(2,-1).transpose(1,0)
+    instance_final = np.zeros_like(binary_pred)
+    embedding_dim = int(instance_pred.shape[1])
+    instance_pred = instance_pred.squeeze().detach().cpu().numpy() #(embedding dim , 368, 640)
+    binary_mask = np.stack([binary_pred for i in range(instance_pred.shape[0])], axis=0)
+    mask = instance_pred * binary_mask #(2, 368, 640)
+    lanes = instance_pred[binary_mask > 0].reshape(embedding_dim,-1).transpose(1,0)
 
     start_clustering = time.time()
-    #clustering = DBSCAN(eps=0.5, min_samples=0).fit(lanes)
-    clustering = MeanShift(n_jobs=4).fit(lanes)
+    #clustering = DBSCAN(eps=0.25, min_samples=8,n_jobs=-1).fit(lanes)
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=150,
+                                allow_single_cluster=False)
+    cluster_labels = clusterer.fit_predict(lanes)
+    #clustering = MeanShift(n_jobs=4).fit(lanes)
     end_clustering = time.time()
-    print(clustering.labels_, np.unique(clustering.labels_))
-    plt.scatter(lanes[:,0], lanes[:,1] , c=clustering.labels_)
-    plt.show()
+
+    #print(cluster_labels, np.unique(cluster_labels))
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111, projection='3d')
+    #ax.scatter(lanes[:,0], lanes[:,1] ,lanes[:,2] , c=cluster_labels)
+    #plt.show()
+
+    print(instance_pred.shape, mask.shape, lanes.shape ,instance_final[binary_pred > 0].shape, cluster_labels.shape)
+    instance_final[binary_pred > 0] = cluster_labels + 1
+    #(4, 368, 640) (4, 368, 640) (42646,) (21323,)
 
     img = img.squeeze().permute(1,2,0).squeeze().cpu().numpy()
 
     print("infer time : ",end-start)
     print("clustering time : ",end_clustering-start_clustering)
-    plt.subplot(4,1,1)
+    plt.subplot(3,1,1)
     plt.imshow(img)
-    plt.subplot(4,1,2)
+    plt.subplot(3,1,2)
     plt.imshow(binary_pred)
-    plt.subplot(4,1,3)
-    plt.imshow(mask[0,:,:])
-    plt.subplot(4,1,4)
-    plt.imshow(mask[1,:,:])
+    plt.subplot(3,1,3)
+    plt.imshow(instance_final)
     plt.show()
